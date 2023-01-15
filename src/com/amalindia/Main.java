a052adf3dd9587c15410c3644c5f774f17948a96
Neena Augustine <neenaaugustine805@gmail.com>
2023-01-15 13:06:34 +0530

    Initial commit

---
+++ src/com/amalindia/Main.java
@@ -0,0 +1,43 @@
+package com.amalindia;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+//        primitive types
+//        byte myAge = 127;
+//        int viewsCount = 123456789;
+//        double viewsCount2 = 98_123_456_789L;
+//        float price = 11.7f;
+//        char letter = 'A';
+//        boolean isEligible = false;
+//
+//        System.out.println(isEligible);
+
+//        reference types
+        // allocate memory to varible as new
+//        Date now = new Date();
+////        now.getTime();
+//        System.out.println(now);
+//        byte x = 1;
+//        byte y = x;
+//        x=8;
+////        System.out.println(x+" "+y);
+//        int x;
+//        Point point1 = new Point(4,9);
+//        Point point2 = point1;
+//        point1.x = 8;
+//        System.out.println(point2);
+////        String message = new String("Hai, This is my string out ");
+//        String message = "Hello World" + "!!";
+//        System.out.println("Hi \"Amal\"");
+//Array declaration
+        int[] numbers = new int[5];
+        numbers[4]=100;
+        numbers[3]=9;
+        System.out.println(numbers);
+
+
+    }
+}
